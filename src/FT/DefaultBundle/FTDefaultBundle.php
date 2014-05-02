7c51d5a1cc9409412c0b3d2b9c61c9f68209c54c
Sylvain CONSO <sconso@e1r4p12.42.fr>
2014-05-02 21:18:32 +0200

    Reset

--- src/FT/DefaultBundle/FTDefaultBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FT\DefaultBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FTDefaultBundle extends Bundle
-{
-}
--- src/FT/MainBundle/FTMainBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FT\MainBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FTMainBundle extends Bundle
-{
-}
